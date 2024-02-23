a75434afd90ba5ada5dacef9f1ba756c418cabb5
h557346 <Nathan.childs@honeywell.com>
2024-02-23 12:50:09 +0000

    initial Commit

--- web.py
+++ web.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import functions
 
+st.set_page_config(layout="wide")
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
@@ -10,8 +11,10 @@ todos = functions.get_todos()
 
 st.title("My Todo App")
 st.subheader("This is my todo App")
-st.write("This app is to increase your productivity")
+st.write("This app is to increase your <b>productivity</b>.",
+         unsafe_allow_html=True)
 
+st.text_input(label="", placeholder="Enter a new todo...", on_change=add_todo, key='new_todo')
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -20,8 +23,5 @@ for index, todo in enumerate(todos):
         functions.write_todos(todos)
         del st.session_state[todo]
         st.experimental_rerun()
-
-
-st.text_input(label="", placeholder="Enter a new todo...", on_change=add_todo, key='new_todo')
 
 st.session_state
