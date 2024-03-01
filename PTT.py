734ac33b9bf81e247e241b45c1a0047feabfcb49
h557346 <Nathan.childs@honeywell.com>
2024-03-01 10:14:31 +0000

    initial Commit

--- PTT.py
+++ PTT.py
@@ -1,4 +1,11 @@
 import streamlit as st
+import pyodbc
+
+# conn = st.connection('pets_db', type='sql')
+# with conn.session as s:
+
+
+
 #import functions
 
 #st.set_page_config(layout="wide")       #Makes Page stretch Left to Right
@@ -10,3 +17,8 @@ st.text_input(label="", placeholder="Scan your H-Number")
 st.text_input(label="", placeholder="Scan the Planned Order Number")
 st.text_input(label="", placeholder="Scan the Line ID")
 
+import streamlit as st
+
+conn = st.experimental_connection('pets_db', type='sql')
+pet_owners = conn.query('select * from pet_owners')
+st.dataframe(pet_owners)
