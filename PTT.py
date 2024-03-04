632b8ef9dae740ff47c8b4e45993afa4f93dd1ed
h557346 <Nathan.childs@honeywell.com>
2024-03-04 09:00:18 +0000

    SQL Connection

--- PTT.py
+++ PTT.py
@@ -1,24 +1,42 @@
 import streamlit as st
 import pyodbc
+import psycopg2
 
-# conn = st.connection('pets_db', type='sql')
-# with conn.session as s:
+hostname = 'localhost'
+database = 'Honeywell Data Project'
+username = 'postgres'
+pwd = 'password'
+port_id = 5432
+conn = None
+cur = None
 
+try:
+    conn = psycopg2.connect(
+                host = hostname,
+                dbname = database,
+                user = username,
+                password = pwd,
+                port = port_id)
 
+    cur = conn.cursor()
 
-#import functions
+except Exception as error:
+    print(error)
+
+finally:
+    if cur is not None:
+        cur.close()
+    if conn is not None:
+        conn.close()
 
 #st.set_page_config(layout="wide")       #Makes Page stretch Left to Right
 
 st.title("Process Tracking Tool")
-st.write("This app is used to provide <b>Traceability</b> for process.",
+st.write("This app is used to provide <b>Traceability</b> for the manufacturing process.",
          unsafe_allow_html=True)
 st.text_input(label="", placeholder="Scan your H-Number")
 st.text_input(label="", placeholder="Scan the Planned Order Number")
 st.text_input(label="", placeholder="Scan the Line ID")
+st.text_input(label="", placeholder="Scan the Products Serial Number")
 
-import streamlit as st
 
-conn = st.experimental_connection('pets_db', type='sql')
-pet_owners = conn.query('select * from pet_owners')
-st.dataframe(pet_owners)
